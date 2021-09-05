f460c23797cc3bc0bc7aa6493d1981643c7f6eac
= <joao.souza@avenuecode.com>
2021-09-05 13:17:03 -0300

    app vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,87 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    </div>
-    <router-view />
+    <Header />
+    <main id="main">
+      <router-view />
+    </main>
+    <Footer />
   </div>
 </template>
 
-<style>
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+:root{
+  --primary-color:#87f;
+  --secondary-color:#65d;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0px;
+  margin: 0px;
+}
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background-color: var(--primary-color);
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 7px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  cursor: pointer;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+}
+
+.btn:hover {
+  color: var(--secondary-color);
+  transform: scale(1.1);
+}
+
+#app{
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+#main{
+  flex:1;
+}
 </style>
